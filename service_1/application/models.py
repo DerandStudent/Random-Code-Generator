22d76756a9f57b6378931dc6fca40258e4bc1d74
Derand Demaj <thederand@gmail.com>
2021-03-02 06:14:05 +0000

    Message ("change to service 1")

--- service_1/application/models.py
+++ service_1/application/models.py
@@ -1,14 +1,6 @@
 from service_1 import db
 
 # classes of db model
-
-
 class Codes(db.Model):
     code_id = db.Column(db.Integer, primary_key=True)
     code = db.Column(db.Text, nullable=False)
-
-# make sur ethat the code is in string format
-
-
-def __repr__(self):
-    return f"Code('{self.code_id}','{self.code}')"
